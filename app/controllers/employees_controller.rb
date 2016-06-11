7c6864ced29141e22d80e535ccb4f44fa2016de6
Nicholas Bendas-Giano <nick@damico.construction>
2016-06-11 18:24:53 -0400

    Completed ADD NEW EMPLOYEE feature

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,9 @@
 class EmployeesController < ApplicationController
+include EmployeesHelper
+
+def new
+	@employee = Employee.new
+end
 
 def index 
 	@employees = Employee.all
@@ -7,6 +12,13 @@ end
 def show
 	@employee = Employee.find(params[:id])
 
+end
+
+def create
+	@employee = Employee.new(employee_params)
+	@employee.save
+
+	redirect_to employee_path(@employee)
 end
 
 
--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,8 @@
 module EmployeesHelper
+
+def employee_params
+	params.require(:employee).permit(:first, :last, :hrly_cost)
+end
+
+
 end
