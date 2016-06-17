d122cedb5bf754b010fd2d6e1230659c30f0885c
Nick Bendas <nick@damico.construction>
2016-06-17 12:37:37 -0400

    Shorted create prompt. Updated labels. Formatted

--- app/controllers/paving_bids_controller.rb
+++ app/controllers/paving_bids_controller.rb
@@ -15,7 +15,34 @@ end
 
 def create
         @paving_bid = PavingBid.new(paving_bid_params)
-        @paving_bid.save
+		@paving_bid.taxRate = 1.0635
+        @paving_bid.tax = 'true'
+
+        sy = @paving_bid.squareFeet/9
+
+        if sy < 3000
+
+                @paving_bid.garitySYrate = 0.1
+                @paving_bid.damicoReclaimTime = 0.5
+
+        elsif sy < 5000
+
+                @paving_bid.garitySYrate = 1
+                @paving_bid.damicoReclaimTime = 1
+
+        else
+                @paving_bid.garitySYrate = 10
+                @paving_bid.damicoReclaimTime = 10
+        end
+
+        @paving_bid.gradeCompactSFrate = 0.20
+        @paving_bid.damicoReclaimCost = 6000
+        @paving_bid.classTwoCost = 76
+        @paving_bid.classOneCost = 70
+        @paving_bid.processCost = 14
+        @paving_bid.levelingCost = 80
+
+		@paving_bid.save
 
         redirect_to paving_bid_path(@paving_bid)
 end
